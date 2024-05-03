827eee7529adfd2f74fdd1d0dafe23b64f1e098c
Alper Değre <alper.degre.dev@gmail.com>
2024-05-03 15:44:40 +0300

    Add new components for info, add a time image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import Dropdown from "./dropdown";
+import KeyboardInfo from "@/components/keyboardinfo";
+import Dropdown from "../components/dropdown";
+import ProjectInfo from "@/components/projectinfo";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1>Pickle Rick!</h1>
+    <main className="flex min-h-screen flex-col items-center p-10 md:p-24 gap-4 relative">
+      <h1 className="text-2xl tracking-wider">Rick & Morty Dropdown</h1>
       <Dropdown />
+      <div className="flex flex-col gap-4 py-10">
+        <KeyboardInfo />
+        <ProjectInfo />
+      </div>
     </main>
   );
 }
